d25ab13848c535e393629f340c47dba03342bfc4
Roody_Wang <roody_wang@pxmart.com.tw>
2024-09-09 11:02:20 +0800

    加入資料轉換

--- CoindeskApi/Controllers/CoindeskController.cs
+++ CoindeskApi/Controllers/CoindeskController.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Globalization;
+using System.Text.Json;
 using CoindeskApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,12 @@ namespace CoindeskApi.Controllers;
 public class CoindeskController : ControllerBase
 {
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly CurrencyContext _context;
 
-    public CoindeskController(IHttpClientFactory httpClientFactory)
+    public CoindeskController(IHttpClientFactory httpClientFactory, CurrencyContext context)
     {
         _httpClientFactory = httpClientFactory;
+        _context = context;
     }
 
     [HttpGet("query")]
@@ -29,6 +32,53 @@ public class CoindeskController : ControllerBase
 
         await using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
         var coindesk = await JsonSerializer.DeserializeAsync<BitcoinPriceIndex>(contentStream);
-        return Ok(coindesk);
+        
+        var dateTime  = DateTime.ParseExact(coindesk.Time.Updated, "MMM d, yyyy HH:mm:ss 'UTC'", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+
+        // TODO 排序, 反射
+        var vo = new RateVo
+        {
+            UpdateTime = dateTime.ToString("yyyy/MM/dd HH:mm:ss"),
+            Currencies = new List<RateVo.Currency>()
+        };
+        var currency1 = new RateVo.Currency
+        {
+            Code = "USD",
+            Name = _context.Currencies.FirstOrDefault(x => x.Code == "USD").CurrencyName,
+            Rate = coindesk.Bpi.USD.RateFloat
+        };
+        vo.Currencies.Add(currency1);
+        
+        var currency2 = new RateVo.Currency
+        {
+            Code = "GBP",
+            Name = _context.Currencies.FirstOrDefault(x => x.Code == "GBP").CurrencyName,
+            Rate = coindesk.Bpi.GBP.RateFloat
+        };
+        vo.Currencies.Add(currency2);
+        
+        var currency3 = new RateVo.Currency
+        {
+            Code = "EUR",
+            Name = _context.Currencies.FirstOrDefault(x => x.Code == "EUR").CurrencyName,
+            Rate = coindesk.Bpi.EUR.RateFloat
+        };
+        vo.Currencies.Add(currency3);
+        
+        return Ok(vo);
+    }
+}
+
+public class RateVo
+{
+    public string UpdateTime { get; set; }
+
+    public List<Currency> Currencies { get; set; }
+
+    public class Currency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public float Rate { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -2,15 +2,46 @@
 面試作業
 
 
-
-create schema coindesk
-go
-
-create table Currency
+```sql
+CREATE TABLE Currency
 (
-Id   int identity,
-Code varchar(3) not null,
-Lang varchar(10) not null,
-CurrencyName nvarchar(10)
+    Id           INT identity,
+    Code         VARCHAR(3)  NOT NULL,
+    Lang         VARCHAR(10) NOT NULL,
+    CurrencyName NVARCHAR(10)
 )
 go
+```
+
+---
+
+任務描述  
+請利用 ASP .NET Core 8.0 建置一個 Web API 專案，實作以下內容，將結果上傳至 GitHub，並提供repo鏈結。  
+
+資料庫  
+SQL Server Express LocalDB（Entity Framework Core）  
+
+功能簡述  
+- 呼叫 coindesk API，解析其下行內容與資料轉換，並實作新的 API。coindesk API：https://api.coindesk.com/v1/bpi/currentprice.json
+- 建立一張幣別與其對應中文名稱的資料表（需附建立SQL語法），並提供查詢/新增/修改/刪除功能 API。
+- 查詢幣別請依照幣別代碼排序。
+
+實作內容  
+- 幣別 DB 維護功能。
+- 呼叫 coindesk 的 API。
+- 呼叫 coindesk 的 API，並進行資料轉換，組成新 API。此新 API 提供：
+  - 更新時間（時間格式範例：1990/01/01 00:00:00）。
+  - 幣別相關資訊（幣別，幣別中文名稱，以及匯率）。
+- 所有功能均須包含單元測試。
+- 將專案上傳至 GitHub 並設為公開分享，回傳repo鏈結。
+- 嘗試錄製demo，上傳至Youtube影片(不要用Shorts)，設為非公開分享回傳
+
+實作加分題 (請於 README 說明包含以下哪些項目)  
+- 印出所有 API 被呼叫以及呼叫外部 API 的 request and response  body log
+- Error handling 處理 API response
+- swagger-ui
+- 多語系設計
+- design pattern 實作
+- 能夠運行在 Docker
+- 加解密技術應用 (AES/RSA…etc.)
+
